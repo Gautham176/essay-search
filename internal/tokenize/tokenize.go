4bf01a193e07980b1093ef6f7cb4d516c365333e
Gautham176 <gautham171206@gmail.com>
2026-05-24 02:47:59 -0700

    Expanded stopwords and edge cases

--- internal/tokenize/tokenize.go
+++ internal/tokenize/tokenize.go
@@ -8,18 +8,38 @@ import (
 // Stopwords is the set of common English words we ignore during indexing.
 // These words appear in almost every document so they carry no signal.
 var Stopwords = map[string]bool{
-	"a": true, "an": true, "the": true,
-	"and": true, "or": true, "but": true, "nor": true,
-	"in": true, "on": true, "at": true, "to": true, "for": true,
-	"of": true, "with": true, "by": true, "from": true, "as": true,
-	"is": true, "was": true, "are": true, "were": true, "be": true,
-	"been": true, "being": true, "have": true, "has": true, "had": true,
-	"do": true, "does": true, "did": true, "will": true, "would": true,
-	"could": true, "should": true, "may": true, "might": true,
-	"it": true, "its": true, "this": true, "that": true, "these": true,
-	"those": true, "i": true, "you": true, "he": true, "she": true,
-	"we": true, "they": true, "what": true, "which": true, "who": true,
-	"not": true, "no": true, "so": true, "if": true, "then": true,
+    // articles, conjunctions, prepositions
+    "a": true, "an": true, "the": true,
+    "and": true, "or": true, "but": true, "nor": true, "yet": true,
+    "in": true, "on": true, "at": true, "to": true, "for": true,
+    "of": true, "with": true, "by": true, "from": true, "as": true,
+    "into": true, "onto": true, "upon": true, "over": true, "under": true,
+    "about": true, "against": true, "between": true, "through": true,
+    "during": true, "before": true, "after": true, "above": true, "below": true,
+
+    // be / have / do / modals
+    "is": true, "was": true, "are": true, "were": true, "be": true,
+    "been": true, "being": true, "am": true,
+    "have": true, "has": true, "had": true, "having": true,
+    "do": true, "does": true, "did": true, "doing": true, "done": true,
+    "will": true, "would": true, "could": true, "should": true,
+    "may": true, "might": true, "must": true, "can": true, "shall": true,
+
+    // pronouns / determiners
+    "it": true, "its": true, "this": true, "that": true, "these": true,
+    "those": true, "i": true, "you": true, "he": true, "she": true,
+    "we": true, "they": true, "them": true, "us": true, "him": true, "her": true,
+    "my": true, "your": true, "our": true, "their": true, "his": true,
+    "what": true, "which": true, "who": true, "whom": true, "whose": true,
+
+    // common adverbs / connectives
+    "not": true, "no": true, "so": true, "if": true, "then": true,
+    "than": true, "such": true, "also": true, "just": true, "very": true,
+    "more": true, "most": true, "less": true, "least": true,
+    "there": true, "here": true, "when": true, "where": true, "why": true,
+    "how": true, "all": true, "any": true, "some": true, "each": true,
+    "every": true, "other": true, "another": true, "same": true,
+    "like": true, "only": true, "own": true, "even": true,
 }
 
 func Tokenize(text string) []string {
